ddc3929a5fc7c084d8423de1e3ffcfba71649fa9
Gaël Péfourque <gaelpef@icloud.com>
2025-08-02 11:27:50 +0200

    debugging chromium linux

--- main.py
+++ main.py
@@ -9,9 +9,8 @@ dc_day_of_week = dc_time.strftime("%A")
 dc_hour = dc_time.hour
 
 if dc_hour:#dc_hour>9 or dc_hour<1:
-    driver = create_chrome_driver(headless=False)
+    driver = create_chrome_driver(headless=True)
     for pizzeria, url in near_pizzerias.items():
         live_traffic, historical_traffic = get_live_traffic(driver, url)
         log_traffic_data(pizzeria, dc_time_string, dc_day_of_week, dc_hour, live_traffic, historical_traffic)
-        break
     driver.quit()
